503d38f241ceca0a9246f335fe98f7c81ea1db6c
Benjamin Pang <benjinpang@gmail.com>
2018-10-27 21:57:20 +0400

    blog 2

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"When I was a middle-schooler, I came up with the idea to create miniscule trackers that I could stick onto any of my belongings and track from my phone or computer. In 2018, this idea sounds rather easy to implement. But back then, the iPhone 5 and Samsung Galaxy S3...","categories": [],
         "tags": [],
         "url": "https://dhs.bennyboy.hosting.nyu.edu/2018/10/04/spatial-perspectives.html",
+        "teaser":null},{
+        "title": "Reflection: Torn Apart/Separados",
+        "excerpt":"Torn Apart/Separados is a project that aims to reveal the deep web of corporate connections that the US Immigration and Customs Enforcement (ICE) has built. From data collected between 2014 to 2018 about corporation-ICE contracts, a team of scholars and activists have created a variety of vivid visualizations depicting various...","categories": [],
+        "tags": [],
+        "url": "https://dhs.bennyboy.hosting.nyu.edu/2018/10/25/project.html",
         "teaser":null}]
